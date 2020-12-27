000a68dee0ead8741130047fc83723602649b66d
Ivan Korotkov <pogomgn@gmail.com>
2020-12-28 02:30:55 +0500

    25.12.20

--- index.php
+++ index.php
@@ -9,103 +9,122 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<div class="hw8">
+<div class="hw9">
     <?
-        function randarr($N, $min = 0, $max = 100) {
-            return array_map(
-                function () use ($min, $max) {
-                    return mt_rand($min, $max);
-                },
-                array_pad([], $N, 0)
-            );
-        }
+        // 5-ая задача и доп задача - в WhatsApp'e
 
-        // 5ая задача
-
-        $arr5 = array(
-            "dim1_1" => array(
-                "dim2_1" => array(
-                    "dim3_1" => "val1",
-                    "dim3_2" => "val2"
-                ),
-                "dim2_2" => array(
-                    "dim3_3" => "val3",
-                    "dim3_4" => "val4"
-                )
-            ),
-            "dim1_2" => array(
-                "dim2_1" => array(
-                    "dim3_1" => "val5",
-                    "dim3_2" => "val6"
-                ),
-                "dim2_2" => array(
-                    "dim3_3" => "val7",
-                    "dim3_4" => "val8"
-                )
-            )
-        );
-        $arr5_res = array();
-
-        echo "<pre>";
-        print_r($arr5);
-        echo "</pre>";
-
-        foreach ($arr5 as $k1 => $v1) {
-            foreach ($v1 as $k2 => $v2) {
-                foreach ($v2 as $k3 => $v3) {
-                    $arr5_res[$k3][$k2][$k1] = $v3;
-                }
-            }
-        }
-
-        echo "<pre>";
-        print_r($arr5_res);
-        echo "</pre>";
-
-        // 23.12.20
+        // 25.12.20
 
         echo "<h2> 1 </h2>";
 
-        $n = 25;
-        $arr1 = randarr($n, 0, 50);
+        $str1 = "some string";
+        if (strlen($str1) > 5)
+            $str1 = mb_substr($str1, 5) . "...";
 
-        $odd = false;
-
-        foreach ($arr1 as $v) {
-            if (!$odd) echo "<p>$v</p>";
-            else echo "<p><b>$v</b></p>";
-            $odd = !$odd;
-        }
+        echo "<p>$str1</p>";
 
         echo "<h2> 2 </h2>";
 
-        $arr2 = [
-            ["ADF", "BCD", "EDZ", "ANV"],
-            ["NOC", "AMG", "BOF", "WOW", "CDG"],
-            ["MOK", "APG", "ALQ"]
-        ];
-
-        foreach ($arr2 as $k1 => $v1) {
-            foreach ($v1 as $k2 => $v2) {
-                if ($v2[0] == 'A') echo "arr2[$k1][$k2] = $v2<br/>";
-            }
-        }
+        $str2 = "asoubasfuabwlubcaiwlyebcaiywbcwaybbwcabwlecbawecb";
+        echo "<p>$str2</p>";
+        $str2 = str_replace(["a", "b", "c"], ["1", "2", "3"], $str2);
+        echo "<p>$str2</p>";
 
         echo "<h2> 3 </h2>";
 
-        $arr3 = [[], [], [], []];
-        foreach ($arr3 as &$subarr) {
-            $subarr = randarr(mt_rand(4, 12), 0, 20);
+        $str3 = "abc abc abc";
+        echo "<p>" . strrpos($str3, "b") . "</p>";
+
+        echo "<h2> 4 </h2>";
+
+        $str4 = "html css php";
+        $array4 = explode(" ", $str4);
+
+        echo "<h2> 5 </h2>";
+
+        $str5_1 = "10-02-2015";
+        $str5_2 = "30-12-2019";
+
+        echo "<p>Дни от $str5_1 до $str5_2</p>";
+
+        $diff = 0;
+
+        $day1 = (int)substr($str5_1, 0, 2);
+        $month1 = (int)substr($str5_1, 3, 2);
+        $year1 = (int)substr($str5_1, 6, 4);
+        $day2 = (int)substr($str5_2, 0, 2);
+        $month2 = (int)substr($str5_2, 3, 2);
+        $year2 = (int)substr($str5_2, 6, 4);
+
+        function isLeap1($year) {
+            return date('L', mktime(0, 0, 0, 1, 1, $year));
         }
 
-        $sum = 0;
-        foreach ($arr3 as $k => $subarr) {
-            $size = count($subarr);
-            $sum += $size;
-            echo "Количество элементов в подмассиве $k = " . $size."<br/>";
+        function isLeap2($year) { // без date()
+            return (!($year % 4) && ($year % 100)) || (!($year % 400));
         }
-        echo "Общее количество элементов - " . $sum."<br/>";
 
+        if (!isLeap2($year1) && $day1 == 29 && $month1 == 2) {
+            die("Нет такой начальной даты!");
+        }
+        if (!isLeap1($year2) && $day2 == 29 && $month2 == 2) {
+            die("Нет такой конечной даты!");
+        }
+
+        $days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // 0 в начале чтобы порядковый номер
+                                                                              // месяца соответствовал номеру в массиве
+
+        $check_leap_start = false;
+        $check_leap_end = false;
+
+        if ($month1 == 1 || $month1 == 2 && $day1 <= 28) $check_leap_start = true;
+        if ($month2 > 2 || $month2 == 2 && $day2 == 29) $check_leap_end = true;
+
+        echo "<p>Проверка начального года на високосность: " . ($check_leap_start ? "да" : "нет") . "</p>";
+        echo "<p>Проверка конечного года на високосность: " . ($check_leap_end ? "да" : "нет") . "</p>";
+
+        $start_year = $check_leap_start ? $year1 : $year1 + 1;
+        $end_year = $check_leap_end ? $year2 + 1 : $year2;
+
+        echo "<p>";
+        for ($i = $start_year; $i < $end_year; $i++) { // посчитаем вошедшие високосные дни
+            echo $i;
+            if (isLeap1($i)) {
+                $diff++;
+                echo "<b>!</b>";
+            }
+            echo " ";
+        }
+        echo "</p>";
+
+        $diff += 365 * ($year2 - $year1 + 1);
+        echo "<p>Добавили дни за года (" . ($year2 - $year1 + 1) . "): $diff</p>";
+
+        $sub1 = 0; // дней в месяцах предшествующих указанному в нач дате
+        for ($i = 1; $i < $month1; $i++) {
+            $sub1 += $days_in_month[$i];
+        }
+        $sub1 += $day1 - 1; // включаем день начальной даты
+        $diff -= $sub1;
+
+        echo "<p>Вычитаем дни которые прошли в году начальной даты ($sub1): $diff</p>";
+
+        $sub2 = 0; // дней в месяцах после указанного в кон дате включая указанный
+        for ($i = $month2; $i <= 12; $i++) {
+            $sub2 += $days_in_month[$i];
+        }
+        $sub2 -= $day2 - 1; // указанный день конечной даты не учитывается
+        $diff -= $sub2;
+                                                                    // Результат  \|/
+        echo "<p>Вычитаем дни которые не прошли в году конечной даты ($sub2): <b>$diff</b></p>";
+
+        // Проверка
+
+        $d1 = new DateTime($str5_1);
+        $d2 = new DateTime($str5_2);
+        $interval = $d1->diff($d2);
+        $res = $interval->format("%a");
+        echo "<p>Проверка: $res</p>";
     ?>
 </div>
 </body>
