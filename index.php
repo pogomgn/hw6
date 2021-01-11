108820438533261d35b3837a7891c60c6ffc3088
Ivan Korotkov <pogomgn@gmail.com>
2021-01-11 11:03:52 +0500

    30.12.20 hw

--- index.php
+++ index.php
@@ -11,120 +11,77 @@
 <body>
 <div class="hw9">
     <?
-        // 5-ая задача и доп задача - в WhatsApp'e
+    echo "<h2>1</h2>";
 
-        // 25.12.20
-
-        echo "<h2> 1 </h2>";
-
-        $str1 = "some string";
-        if (strlen($str1) > 5)
-            $str1 = mb_substr($str1, 5) . "...";
-
-        echo "<p>$str1</p>";
-
-        echo "<h2> 2 </h2>";
-
-        $str2 = "asoubasfuabwlubcaiwlyebcaiywbcwaybbwcabwlecbawecb";
-        echo "<p>$str2</p>";
-        $str2 = str_replace(["a", "b", "c"], ["1", "2", "3"], $str2);
-        echo "<p>$str2</p>";
-
-        echo "<h2> 3 </h2>";
-
-        $str3 = "abc abc abc";
-        echo "<p>" . strrpos($str3, "b") . "</p>";
-
-        echo "<h2> 4 </h2>";
-
-        $str4 = "html css php";
-        $array4 = explode(" ", $str4);
-
-        echo "<h2> 5 </h2>";
-
-        $str5_1 = "10-02-2015";
-        $str5_2 = "30-12-2019";
-
-        echo "<p>Дни от $str5_1 до $str5_2</p>";
-
-        $diff = 0;
-
-        $day1 = (int)substr($str5_1, 0, 2);
-        $month1 = (int)substr($str5_1, 3, 2);
-        $year1 = (int)substr($str5_1, 6, 4);
-        $day2 = (int)substr($str5_2, 0, 2);
-        $month2 = (int)substr($str5_2, 3, 2);
-        $year2 = (int)substr($str5_2, 6, 4);
-
-        function isLeap1($year) {
-            return date('L', mktime(0, 0, 0, 1, 1, $year));
+    function f1($array)
+    {
+        for ($i = 0; $i < count($array); $i++) {
+            $array[$i] = mt_rand(0, 1000);
         }
+        return $array;
+    }
 
-        function isLeap2($year) { // без date()
-            return (!($year % 4) && ($year % 100)) || (!($year % 400));
+    $arr1 = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    echo "<pre>";
+    print_r($arr1);
+    echo "</pre>";
+
+    $arr1 = f1($arr1);
+
+    echo "<pre>";
+    print_r($arr1);
+    echo "</pre>";
+
+    $str = "HTML, CSS, PHP, BITRIX";
+    $symb = [",", ".", "!", "?"];
+
+    echo "<h2>2</h2>";
+
+    function f2($s, $delimiters)
+    {
+        $repl = array_pad([], count($delimiters), "");
+        $s = str_replace($delimiters, $repl, $s);
+        return count(explode(" ", $s));
+    }
+
+    echo "<p>" . f2($str, $symb) . "</p>";
+
+    echo "<h2>3</h2>";
+
+    function f3($s)
+    {
+        $res = "";
+        for ($i = mb_strlen($s) - 1; $i >= 0; $i--) {
+            $res .= mb_substr($s, $i, 1);
         }
+        return $res;
+    }
 
-        if (!isLeap2($year1) && $day1 == 29 && $month1 == 2) {
-            die("Нет такой начальной даты!");
+    echo "<p>" . f3($str) . "</p>";
+
+    echo "<h2>4</h2>";
+
+    function f4($s)
+    {
+        return count(mb_str_split($s, 1)); // php7.4+ или просто return mb_strlen($s);
+    }
+
+    echo "<p>" . f4($str) . "</p>";
+
+    echo "<h2>5</h2>";
+
+    function f5($s)
+    {
+        $res = "";
+        for ($i = 0; $i < mb_strlen($s); $i++) {
+            $res .= mb_substr($s, $i, 1) . "<br/>";
         }
-        if (!isLeap1($year2) && $day2 == 29 && $month2 == 2) {
-            die("Нет такой конечной даты!");
-        }
+        return $res;
+    }
 
-        $days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; // 0 в начале чтобы порядковый номер
-                                                                              // месяца соответствовал номеру в массиве
+    echo "<p>" . f5($str) . "</p>";
 
-        $check_leap_start = false;
-        $check_leap_end = false;
-
-        if ($month1 == 1 || $month1 == 2 && $day1 <= 28) $check_leap_start = true;
-        if ($month2 > 2 || $month2 == 2 && $day2 == 29) $check_leap_end = true;
-
-        echo "<p>Проверка начального года на високосность: " . ($check_leap_start ? "да" : "нет") . "</p>";
-        echo "<p>Проверка конечного года на високосность: " . ($check_leap_end ? "да" : "нет") . "</p>";
-
-        $start_year = $check_leap_start ? $year1 : $year1 + 1;
-        $end_year = $check_leap_end ? $year2 + 1 : $year2;
-
-        echo "<p>";
-        for ($i = $start_year; $i < $end_year; $i++) { // посчитаем вошедшие високосные дни
-            echo $i;
-            if (isLeap1($i)) {
-                $diff++;
-                echo "<b>!</b>";
-            }
-            echo " ";
-        }
-        echo "</p>";
-
-        $diff += 365 * ($year2 - $year1 + 1);
-        echo "<p>Добавили дни за года (" . ($year2 - $year1 + 1) . "): $diff</p>";
-
-        $sub1 = 0; // дней в месяцах предшествующих указанному в нач дате
-        for ($i = 1; $i < $month1; $i++) {
-            $sub1 += $days_in_month[$i];
-        }
-        $sub1 += $day1 - 1; // включаем день начальной даты
-        $diff -= $sub1;
-
-        echo "<p>Вычитаем дни которые прошли в году начальной даты ($sub1): $diff</p>";
-
-        $sub2 = 0; // дней в месяцах после указанного в кон дате включая указанный
-        for ($i = $month2; $i <= 12; $i++) {
-            $sub2 += $days_in_month[$i];
-        }
-        $sub2 -= $day2 - 1; // указанный день конечной даты не учитывается
-        $diff -= $sub2;
-                                                                    // Результат  \|/
-        echo "<p>Вычитаем дни которые не прошли в году конечной даты ($sub2): <b>$diff</b></p>";
-
-        // Проверка
-
-        $d1 = new DateTime($str5_1);
-        $d2 = new DateTime($str5_2);
-        $interval = $d1->diff($d2);
-        $res = $interval->format("%a");
-        echo "<p>Проверка: $res</p>";
     ?>
 </div>
 </body>
