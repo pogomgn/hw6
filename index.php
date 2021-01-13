1aeb29b84ec556615215b2933ad2d45b1c9322ef
Ivan Korotkov <pogomgn@gmail.com>
2021-01-13 11:05:08 +0500

    11.01.21 HW

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+if (isset($_GET["goto"])){
+    $gt = addslashes($_GET["goto"]);
+    header("location: phpcourse.php?l=$gt");
+}
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -10,79 +16,14 @@
 </head>
 <body>
 <div class="hw9">
-    <?
-    echo "<h2>1</h2>";
-
-    function f1($array)
-    {
-        for ($i = 0; $i < count($array); $i++) {
-            $array[$i] = mt_rand(0, 1000);
-        }
-        return $array;
-    }
-
-    $arr1 = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-    echo "<pre>";
-    print_r($arr1);
-    echo "</pre>";
-
-    $arr1 = f1($arr1);
-
-    echo "<pre>";
-    print_r($arr1);
-    echo "</pre>";
-
-    $str = "HTML, CSS, PHP, BITRIX";
-    $symb = [",", ".", "!", "?"];
-
-    echo "<h2>2</h2>";
-
-    function f2($s, $delimiters)
-    {
-        $repl = array_pad([], count($delimiters), "");
-        $s = str_replace($delimiters, $repl, $s);
-        return count(explode(" ", $s));
-    }
-
-    echo "<p>" . f2($str, $symb) . "</p>";
-
-    echo "<h2>3</h2>";
-
-    function f3($s)
-    {
-        $res = "";
-        for ($i = mb_strlen($s) - 1; $i >= 0; $i--) {
-            $res .= mb_substr($s, $i, 1);
-        }
-        return $res;
-    }
-
-    echo "<p>" . f3($str) . "</p>";
-
-    echo "<h2>4</h2>";
-
-    function f4($s)
-    {
-        return count(mb_str_split($s, 1)); // php7.4+ или просто return mb_strlen($s);
-    }
-
-    echo "<p>" . f4($str) . "</p>";
-
-    echo "<h2>5</h2>";
-
-    function f5($s)
-    {
-        $res = "";
-        for ($i = 0; $i < mb_strlen($s); $i++) {
-            $res .= mb_substr($s, $i, 1) . "<br/>";
-        }
-        return $res;
-    }
-
-    echo "<p>" . f5($str) . "</p>";
-
-    ?>
+    <h2>Задание 3</h2>
+    <form method="get">
+        <select name="goto">
+            <option value="1">Дз № 2</option>
+            <option value="2">Дз № 3</option>
+        </select>
+        <button type="submit">Перейти</button>
+    </form>
 </div>
 </body>
 </html>
---
+++ phpcourse.php
@@ -0,0 +1,150 @@
+<?php
+$page = -1;
+if (isset($_GET["l"])) {
+    $page = $_GET["l"];
+}
+
+$signin = false; // флаг попытки залогиниться
+$admin = false; // флаг совпадения логопасса с админскими
+
+if (isset($_POST["login"]) && isset($_POST["password"])) {
+    $signin = true;
+    if ("admin" == $_POST["login"] && "962012d09b8170d912f0669f6d7d9d07" == $_POST["password"]) {
+        $admin = true;
+    }
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="hw9">
+
+    <!-- ############## ДЗ 2 ############## -->
+
+    <? if (1 == $page) : ?>
+        <h2>№ 2</h2>
+        <p>
+            <?
+            if ($admin) {
+                echo "Привет админ!!";
+            } else {
+                if ($signin) {
+                    echo "Неверный логин или пароль";
+                } else {
+                    echo "для входа admin'ом - пароль &quot;qwer&quot;";
+                }
+            }
+            ?>
+        </p>
+        <form id="form1" method="post">
+            <input type="text" name="login" placeholder="Enter login"/>
+            <input type="password" id="pwd" name="password" placeholder="Enter password"/>
+
+            <button type="submit">Sign in</button>
+        </form>
+        <script src="http://www.myersdaily.org/joseph/javascript/md5.js"></script>
+        <script>
+            let form = document.getElementById("form1");
+
+            function submitHandler() {
+                let md5hash = md5(document.getElementById("pwd").value);
+                document.getElementById("pwd").value = md5hash;
+            }
+
+            form.addEventListener("submit", submitHandler);
+        </script>
+    <? endif; ?>
+
+    <!-- ############## ДЗ 3 ############## -->
+
+    <? if (2 == $page) : ?>
+        <h2>№ 3</h2>
+        <?
+        $results = false; // флаг просмотра результатов
+
+        if (isset($_GET["name"])) {
+            $results = true;
+        }
+
+        // читаем список вопросов
+
+        $questions = [];
+        $values = [];
+        $handle = fopen("questions.txt", "r");
+        if ($handle) {
+            while (($q = fgets($handle)) !== false) {
+                $tmp = explode("#", $q);
+                $questions[] = $tmp[1];
+                $values[] = $tmp[0];
+            }
+            fclose($handle);
+        } else {
+            echo "Что-то пошло не так :(";
+        }
+        ?>
+
+        <? if (!$results) : ?>
+            <form method="get">
+                <input type="hidden" name="l" value="2"/> <!-- сохраняем страницу для отправки -->
+                <input type="text" name="name" required placeholder="Введите ваше имя"/>
+                <?
+                for ($i = 0; $i < count($questions); $i++) {
+                    echo "
+                <fieldset>
+                  <legend>$questions[$i]</legend>
+                  <div>
+                    <input type=\"radio\" id=\"q1_$i\" name=\"q[$i]\" value=\"1\">
+                    <label for=\"q1_$i\">Да</label>
+                  </div>
+                  <div>
+                    <input type=\"radio\" id=\"q2_$i\" name=\"q[$i]\" value=\"0\">
+                    <label for=\"q2_$i\">Нет</label>
+                  </div>
+                </fieldset>
+                    ";
+                }
+                ?>
+                <button type="submit">Отправить</button>
+            </form>
+        <? else: ?>
+
+            <p><?=$_GET["name"]?>, вы прошли анкету! Ваш результат:</p>
+            <p>
+            <?
+            $ans = $_GET["q"];
+            $points = 0;
+            for ($i=0;$i<count($ans);$i++) {
+                if ($ans[$i] != $values[$i]){
+                    $points++;
+                    echo "1";
+                } else{
+                    echo "0";
+                }
+            }
+            echo " = $points баллов";
+            ?>
+            </p>
+            <p>
+            <?
+            if ($points > 15) echo "У Вас покладистый характер";
+            elseif ($points >= 8) echo "Вы не лишены недостатков, но с вами можно ладить";
+            else echo "Вашим друзьям можно посочувствовать";
+            ?>
+            </p>
+            <a href="phpcourse.php?l=2">Пройти заново</a>
+
+        <? endif; ?>
+
+    <? endif; ?>
+
+</div>
+</body>
+</html>
